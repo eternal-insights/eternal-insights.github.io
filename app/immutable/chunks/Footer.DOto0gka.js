49251b1ccc501859afc20a40642c63728a8995b1
joshtune <joshua.tune@gmail.com>
2024-10-06 20:09:00 -0600

    auto deployment updates

---
+++ app/immutable/chunks/Footer.DOto0gka.js
@@ -0,0 +1,12 @@
+import{s as A,e as V,n as I}from"./scheduler.C_GhXtWQ.js";import{S as k,i as q,e as o,b as w,s as F,c as i,d as m,f as x,h as G,r as M,g as h,q as r,j as Y,k as s}from"./index.D7ipX5tW.js";function C(f){let t,l,c,e,n,D,p,g,_,y,b,d,E=`<strong>Disclaimer</strong>
+					The content on this website includes text that has been generated using AI tools, including
+					GPT (by OpenAI) and Recorder by Google. While every effort has been made to ensure the accuracy
+					and relevance of the information provided, the nature of AI-generated content means that it
+					may not always fully reflect the views or knowledge of human authors or experts.<br/>
+
+					We recommend that users verify any critical information independently and consult
+					appropriate professionals for specific advice or guidance. The owners of this website are
+					not responsible for any inaccuracies or misinterpretations that may arise from the use of
+					AI-generated content.`;return{c(){t=o("div"),l=o("div"),c=o("div"),e=o("div"),n=o("img"),p=w(`
+				© `),g=w(f[0]),_=w(" | Eternal Insights"),y=o("br"),b=F(),d=o("p"),d.innerHTML=E,this.h()},l(u){t=i(u,"DIV",{id:!0,class:!0});var v=m(t);l=i(v,"DIV",{class:!0});var j=m(l);c=i(j,"DIV",{class:!0});var T=m(c);e=i(T,"DIV",{class:!0});var a=m(e);n=i(a,"IMG",{alt:!0,src:!0,class:!0}),p=x(a,`
+				© `),g=x(a,f[0]),_=x(a," | Eternal Insights"),y=i(a,"BR",{}),b=G(a),d=i(a,"P",{"data-svelte-h":!0}),M(d)!=="svelte-kb8dlr"&&(d.innerHTML=E),a.forEach(h),T.forEach(h),j.forEach(h),v.forEach(h),this.h()},h(){r(n,"alt","logo"),V(n.src,D="/assets/logo.png")||r(n,"src",D),r(n,"class","footer-logo svelte-ujhjs7"),r(e,"class","col"),r(c,"class","row"),r(l,"class","container pt-5"),r(t,"id","layout-footer"),r(t,"class","bg-body-tertiary d-print-none svelte-ujhjs7")},m(u,v){Y(u,t,v),s(t,l),s(l,c),s(c,e),s(e,n),s(e,p),s(e,g),s(e,_),s(e,y),s(e,b),s(e,d)},p:I,i:I,o:I,d(u){u&&h(t)}}}function H(f){return[new Date().getFullYear()]}class R extends k{constructor(t){super(),q(this,t,H,C,A,{})}}export{R as F};
--- app/immutable/entry/start.DoTOL-M9.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Fp3FZKTH.js";export{t as start};
